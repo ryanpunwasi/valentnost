c2cade10b81192f2742da89d6ac6d4b0f832b803
Rx <ryandeonaraine@gmail.com>
2022-04-04 17:45:48 -0400

    Added config file for developer links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import {EMAIL, TWITTER} from '../config/links';
 
 import './Nav.css';
 
@@ -14,7 +15,7 @@ const Nav = () => {
         <ul className='nav-list'>
           <li className='home'><Link to='/'><i className="fa-solid fa-vial"></i></Link></li>
           <li className='support'><a href="https://www.buymeacoffee.com/bryndenrivers" target='_blank' rel='noreferrer'><i className="fa-regular fa-heart"></i></a></li>
-          <li className='twitter'><a href='https://twitter.com/BryndenDevant' target='_blank' rel='noreferrer'><i className="fa-solid fa-hashtag"></i></a></li>
+          <li className='twitter'><a href={TWITTER} target='_blank' rel='noreferrer'><i className="fa-solid fa-hashtag"></i></a></li>
         </ul>
       </nav>
     </header>
---
+++ src/config/links.js
@@ -0,0 +1,2 @@
+export const EMAIL = 'ryanpunwasi@gmail.com';
+export const TWITTER = 'https://twitter.com/RyanPunwasi';
