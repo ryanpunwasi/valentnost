456496d8772a809b87b59a0bd33d52349d42efa6
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-23 01:36:58 -0400

    Prettier formatting

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { 
+import {
   SELECT_ELEMENT,
   FETCH_TABLE,
   SELECT_CHEMICAL_GROUP,
@@ -8,84 +8,84 @@ import {
   START_PRACTICE,
   CHECK_ANSWER,
   NEXT_QUESTION,
-  END_PRACTICE
+  END_PRACTICE,
 } from "./types";
 
-import history from '../history';
-import periodicTable from '../apis/periodicTable';
-import { buildElementsFromGroup } from '../utils/buildElementsFromGroup';
+import history from "../history";
+import periodicTable from "../apis/periodicTable";
+import { buildElementsFromGroup } from "../utils/buildElementsFromGroup";
 
 export const selectElement = element => {
   return {
     type: SELECT_ELEMENT,
-    payload: element
-  }
-}
+    payload: element,
+  };
+};
 
-export const fetchTable =  () => {
-  return async function(dispatch, getState) {
-    const response = await periodicTable.get('/');
-  
+export const fetchTable = () => {
+  return async function (dispatch, getState) {
+    const response = await periodicTable.get("/");
+
     dispatch({ type: FETCH_TABLE, payload: response });
-  }
-}
+  };
+};
 
-export const selectChemicalGroup = (group) => {
+export const selectChemicalGroup = group => {
   return {
     type: SELECT_CHEMICAL_GROUP,
-    payload: group
-  }
-}
+    payload: group,
+  };
+};
 
 export const clearForm = () => {
   return {
-    type: CLEAR_FORM
-  }
-}
+    type: CLEAR_FORM,
+  };
+};
 
 export const hideAlert = () => {
   return {
-    type: HIDE_ALERT
-  }
-}
+    type: HIDE_ALERT,
+  };
+};
 
 export const showAlert = () => {
   return {
-    type: SHOW_ALERT
-  }
-}
+    type: SHOW_ALERT,
+  };
+};
 
-export const startPractice = (group, table) => async (dispatch) => {
+export const startPractice = (group, table) => async dispatch => {
   dispatch({
     type: START_PRACTICE,
-    payload: buildElementsFromGroup(group, table)
+    payload: buildElementsFromGroup(group, table),
   });
 
-  history.push('/practice');
-}
+  history.push("/practice");
+};
 
 export const checkAnswer = (isCorrect, currentElement) => {
   return {
     type: CHECK_ANSWER,
     payload: {
-      isCorrect, 
-      currentElement
-    }
-  }
-}
+      isCorrect,
+      currentElement,
+    },
+  };
+};
 
 export const nextQuestion = (isCorrect, currentElement) => {
   return {
     type: NEXT_QUESTION,
     payload: {
       isCorrect,
-      currentElement
-    }
+      currentElement,
+    },
   };
-}
+};
 
 export const endPractice = () => {
   return {
-    type: END_PRACTICE
-  }
-}
+    type: END_PRACTICE,
+  };
+};
