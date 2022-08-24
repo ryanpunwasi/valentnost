5c13d8e8b2cc126bba62451e5152feb160264dd2
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-24 01:17:53 -0400

    Add three instances of mock practice data

---
+++ src/data/practice.js
@@ -0,0 +1,172 @@
+export const practice01 = {
+  hasAnswered: false,
+  answered: 0,
+  correct: 0,
+  incorrect: [],
+  seen: ["Krypton"],
+  currentElement: {
+    name: "Krypton",
+    number: "36",
+    symbol: "Kr",
+  },
+  elements: [
+    {
+      name: "Helium",
+      number: "2",
+      symbol: "He",
+    },
+    {
+      name: "Neon",
+      number: "10",
+      symbol: "Ne",
+    },
+    {
+      name: "Argon",
+      number: "18",
+      symbol: "Ar",
+    },
+    {
+      name: "Krypton",
+      number: "36",
+      symbol: "Kr",
+    },
+    {
+      name: "Xenon",
+      number: "54",
+      symbol: "Xe",
+    },
+    {
+      name: "Radon",
+      number: "86",
+      symbol: "Rn",
+    },
+    {
+      name: "Oganesson",
+      number: "118",
+      symbol: "Og",
+    },
+  ],
+  prevElement: null,
+  answeredCorrect: null,
+  progress: 0,
+};
+
+export const practice02 = {
+  hasAnswered: false,
+  answered: 51,
+  correct: 50,
+  incorrect: [],
+  seen: ["Krypton", "Argon", "Helium", "Oganesson", "Neon", "Xenon", "Radon"],
+  currentElement: {
+    name: "Xenon",
+    number: "54",
+    symbol: "Xe",
+  },
+  elements: [
+    {
+      name: "Helium",
+      number: "2",
+      symbol: "He",
+    },
+    {
+      name: "Neon",
+      number: "10",
+      symbol: "Ne",
+    },
+    {
+      name: "Argon",
+      number: "18",
+      symbol: "Ar",
+    },
+    {
+      name: "Krypton",
+      number: "36",
+      symbol: "Kr",
+    },
+    {
+      name: "Xenon",
+      number: "54",
+      symbol: "Xe",
+    },
+    {
+      name: "Radon",
+      number: "86",
+      symbol: "Rn",
+    },
+    {
+      name: "Oganesson",
+      number: "118",
+      symbol: "Og",
+    },
+  ],
+  prevElement: {
+    name: "Oganesson",
+    number: "118",
+    symbol: "Og",
+  },
+  answeredCorrect: null,
+  progress: 0,
+};
+
+export const practice03 = {
+  hasAnswered: false,
+  answered: 35,
+  correct: 35,
+  incorrect: [
+    {
+      name: "Helium",
+      number: "2",
+      symbol: "He",
+    },
+  ],
+  seen: ["Krypton", "Argon", "Helium", "Oganesson", "Xenon"],
+  currentElement: {
+    name: "Xenon",
+    number: "54",
+    symbol: "Xe",
+  },
+  elements: [
+    {
+      name: "Helium",
+      number: "2",
+      symbol: "He",
+    },
+    {
+      name: "Neon",
+      number: "10",
+      symbol: "Ne",
+    },
+    {
+      name: "Argon",
+      number: "18",
+      symbol: "Ar",
+    },
+    {
+      name: "Krypton",
+      number: "36",
+      symbol: "Kr",
+    },
+    {
+      name: "Xenon",
+      number: "54",
+      symbol: "Xe",
+    },
+    {
+      name: "Radon",
+      number: "86",
+      symbol: "Rn",
+    },
+    {
+      name: "Oganesson",
+      number: "118",
+      symbol: "Og",
+    },
+  ],
+  prevElement: {
+    name: "Oganesson",
+    number: "118",
+    symbol: "Og",
+  },
+  answeredCorrect: null,
+  progress: 0,
+};
