3255e5c65e56d4398964c5d62193e8ca02d8237f
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-25 17:53:03 -0400

    refactor: use destructuring for prop values

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { hideAlert } from '../actions';
-import './Alert.css';
+import React from "react";
+import { connect } from "react-redux";
+import { hideAlert } from "../actions";
+import "./Alert.css";
 
-const Alert = (props) => {
-
-  return(
-    <div className={`alert-container ${ props.alert }`}>
-      <div className='alert-message'>{props.message}</div>
-      <span onClick={props.hideAlert} className='alert-close-icon'><i className="fa-solid fa-xmark"></i></span>
+const Alert = ({ alert, message, hideAlert }) => {
+  return (
+    <div className={`alert-container ${alert}`}>
+      <div className="alert-message">{message}</div>
+      <span onClick={hideAlert} className="alert-close-icon">
+        <i className="fa-solid fa-xmark"></i>
+      </span>
     </div>
   );
-}
-
+};
 
 const mapStateToProps = state => {
-  return { alert: state.alert }
-}
+  return { alert: state.alert };
+};
 
 export default connect(mapStateToProps, { hideAlert })(Alert);
