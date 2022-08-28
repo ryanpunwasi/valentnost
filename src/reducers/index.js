721893b86ad72f97e442badc8f196ccb11f1f7bf
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-27 20:20:04 -0400

    feat: initialize mode state in redux store, write action creator and reducer for updating mode

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,13 @@ import fetchTableReducer from "./fetchTableReducer";
 import selectChemicalGroup from "./selectChemicalGroup";
 import alertReducer from "./alertReducer";
 import practiceReducer from "./practiceReducer";
-
+import modeReducer from "./modeReducer";
 
 export default combineReducers({
-    element: selectElement,
-    table: fetchTableReducer,
-    group: selectChemicalGroup,
-    alert: alertReducer,
-    practice: practiceReducer
+  element: selectElement,
+  table: fetchTableReducer,
+  group: selectChemicalGroup,
+  alert: alertReducer,
+  practice: practiceReducer,
+  mode: modeReducer,
 });
