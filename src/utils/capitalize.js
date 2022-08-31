3776bab8d472fd7a28ee6b8b05b9f9993af9db5a
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-30 22:50:58 -0400

    chore: handle edge cases

--- src/utils/capitalize.js
+++ src/utils/capitalize.js
@@ -1,3 +1,8 @@
 export const capitalize = str => {
-  return str[0].toUpperCase() + str.substring(1);
-}
+  if (!str) {
+    return "";
+  } else if (str.length === 1) {
+    return str[0].toUpperCase();
+  }
+  return str[0].toUpperCase() + str.substring(1).toLowerCase();
+};
