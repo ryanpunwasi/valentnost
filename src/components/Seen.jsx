9d5040fc858d233590fafc4a07ff9746cf15e8b0
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-09-26 22:52:38 -0400

    refactor: remove unnecessary variable assignments

--- src/components/Seen.jsx
+++ src/components/Seen.jsx
@@ -3,9 +3,7 @@ import "./Seen.css";
 
 const Seen = props => {
   const renderElementsSeen = practice => {
-    const totalElements = practice.elements.length;
-    const seenElements = practice.seen.length;
-    return `${seenElements}/${totalElements} seen`;
+    return `${practice.seen.length}/${practice.elements.length} seen`;
   };
 
   return <div className="seen-card">{renderElementsSeen(props.practice)}</div>;
